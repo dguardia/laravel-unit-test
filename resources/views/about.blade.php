925792d7f569507727ee166a19c3a14ef2895ecd
David Guardia <david.guardia@gmail.com>
2019-05-01 20:45:02 -0400

    adding pages

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('title', 'About')
+@section('content')
+<h1>About page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sapiente repudiandae cum iusto quisquam ipsum aliquid, vel est nulla ipsa atque veritatis cumque voluptatibus quibusdam pariatur! Repudiandae fugiat pariatur blanditiis!</p>
+
+@endsection
